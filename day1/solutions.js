62cbeafdfba0f3379c7084d44dbdf6bb24dcf011
Jason Nede <j.r.nede@gillz.nl>
2019-12-08 19:36:59 +0100

    code clean-up

--- day1/solutions.js
+++ day1/solutions.js
@@ -1,23 +1,21 @@
-const input = require("./puzzleInputs");
+const {firstPuzzleInput} = require("./puzzleInputs");
+
+const calculateFuel = (d) => (Math.floor(d / 3) -2);
+
+const helper = (input, accumulator = 0) => {
+    if (calculateFuel(input) < 1) return parseInt(accumulator);
+    let neededFuel = calculateFuel(input);
+    return helper(neededFuel, (accumulator += neededFuel));
+};
 
 exports.puzzleOneSolution = (inputs) => {
-    return inputs.reduce((acc, curr) => acc + (Math.floor(curr / 3) -2), 0);
+    return inputs.reduce((acc, curr) => acc + calculateFuel(curr), 0);
 };
-// console.log('puzzleOneSolution',puzzleOneSolution(input.firstPuzzleInput))
-
-function helper(input, accumulator = 0) {
-    if ((Math.floor(input / 3) -2) < 1 ) {
-        return parseInt(accumulator);
-    }
-    let neededFuel = (Math.floor(input / 3) -2);
-    accumulator += neededFuel;
-    return helper(neededFuel, accumulator);
-}
 
 exports.puzzleTwoSolution = (inputs) => {
     return inputs.reduce((acc, curr) => acc + helper(curr), 0);
 };
 
-console.log('puzzleOneSolution',puzzleOneSolution(input.firstPuzzleInput))
-console.log('puzzleTwoSolution',exports.puzzleTwoSolution(input.firstPuzzleInput));
+console.log('puzzleOneSolution',exports.puzzleOneSolution(firstPuzzleInput));
+console.log('puzzleTwoSolution',exports.puzzleTwoSolution(firstPuzzleInput));
 
